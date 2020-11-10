fab1fe8bcf12cd23f6ee8d3e77c5610b3b656e90
joao <jpoliveira11@gmail.com>
2020-11-10 16:48:18 +0000

    changes on capistrano rails

--- config/deploy/plugins/capistrano_rails.rb
+++ config/deploy/plugins/capistrano_rails.rb
@@ -1,23 +1,16 @@
-# frozen_string_literal: true
-
 namespace :deploy do
   namespace :check do
-    desc 'Upload credentials key files if not present'
-    task :upload_credentials_keys do
+    desc 'Upload master.key file if not present'
+    task :upload_master_key do
       on roles(:app), in: :sequence, wait: 10 do
         unless test("[ -f #{shared_path}/config/master.key ]")
           upload! 'config/master.key', "#{shared_path}/config/master.key"
-        end
-
-        unless test("[ -f #{shared_path}/config/credentials/#{fetch(:stage)}.key ]")
-          execute "mkdir -p #{shared_path}/config/credentials"
-          upload! "config/credentials/#{fetch(:stage)}.key", "#{shared_path}/config/credentials/#{fetch(:stage)}.key"
         end
       end
     end
   end
 end
-before 'deploy:check:linked_files', 'deploy:check:upload_credentials_keys'
+before 'deploy:check:linked_files', 'deploy:check:upload_master_key'
 
-# Symlink the credentials keys from the shared directory.
-append :linked_files, 'config/master.key', "config/credentials/#{fetch(:stage)}.key"
+# Symlink the master key from the shared directory.
+append :linked_files, 'config/master.key'
