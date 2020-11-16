13af27e6f2c82406c9ae63bbcd4c1d16d2011509
joao <jpoliveira11@gmail.com>
2020-11-16 15:11:47 +0000

    new host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 set :branch, ENV.fetch('BRANCH', :main)
-server "ec2-35-180-164-255.eu-west-3.compute.amazonaws.com", user: "deploy", roles: %w(web app db)
+server "ec2-35-181-48-80.eu-west-3.compute.amazonaws.com", user: "deploy", roles: %w(web app db)
